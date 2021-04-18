c1d08758e565a94a9a54d1d2bb8e45454aaf006e
nomanitarique07 <nomanitarique07@gmail.com>
2021-04-18 22:46:01 +0530

    Weather App

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,27 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = '1c7ef8ccbb937e2e3098944f61e590fe';
+const openWeatherMapURL='https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getCityWeather(String cityName)async{
+    NetworkHelper networkHelper=NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData=await networkHelper.getData();
+    return weatherData;
+
+  }
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+    await location.getCurrentLocation();
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
